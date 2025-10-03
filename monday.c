dd8c47067a343c45bf6d68aa39804d45cae05cb8
Lgawaskar077 <lokugawaskar2025@gmail.com>
2025-10-03 02:16:19 -0700

    Add files via upload

---
+++ monday.c
@@ -0,0 +1,42 @@
+/*write a program findout length and width or two any a farm bigger.
+*/
+#include<stdio.h>
+void main ()
+{
+    
+    int week,day;
+
+    printf("Enter Week Number(1-7)");
+    scanf("%d",&week);
+
+    if(week==1)
+    {
+        printf("\n1=Monday");
+    }
+    if(week==2)
+    {
+        printf("\n2=Tuesday");
+    }
+    if(week==3)
+    {
+        printf("\n3=Wednesday");
+    }
+    if(week==4)
+    {
+        printf("\n4=Thursday");
+    }
+    if(week==5)
+    {
+        printf("\n5=Friday");
+    }
+    if(week==6)
+    {
+        printf("\n6=Saturday");
+    }
+    if(week==7)
+    {
+        printf("\n7=Sunday");
+    }
+
+}
+     
